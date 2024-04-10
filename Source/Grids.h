8f551b16e9d919813444193944ccf00dcc80c085
Tugrul Akyuz <tugrulvega@gmail.com>
2024-04-10 18:33:49 +0300

    box colour changed

--- Source/Grids.h
+++ Source/Grids.h
@@ -153,7 +153,7 @@ public:
        
         //auto bC = Colour(0xFFB0C3CF);
         //auto bC = Colour(0xFFAFAD9F);
-        auto bC = Colours::white;
+        auto bC = Colours::lightgoldenrodyellow;
         // Determine the color based on the current state
         switch (currentState)
         {
